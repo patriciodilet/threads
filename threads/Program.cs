bf4d998a5a47e7af5337c8c6d9c42cb6b15b731e
AIEP\Patricio Diaz <Patricio Daz>
2022-08-15 10:10:21 -0400

    Agregar archivos de proyecto.

---
+++ threads/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+namespace threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread obj1 = new Thread(Function1);
+            Thread obj2 = new Thread(Function2);
+            obj1.Start();
+            obj2.Start();
+        }
+
+        static void Function1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 1 exectuted " + i.ToString());
+            }
+        }
+
+        static void Function2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 2 exectued " + i.ToString());
+            }
+        }
+    }
+}
